ac888d8023d5d38268e9407b5b595439b9cb8bca
nikita-iii <timohaflekss@gmail.com>
2026-04-07 21:33:59 +0300

    t-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,79 @@
 // Copyright 2025 UNN-CS
 #include <cstdint>
+#include <stdexcept>
 #include "alg.h"
 
-
-bool checkPrime(uint64_t value) {
-  // вставьте код функции
+bool checkPrime(uint64_t val) {
+  if (val < 2) {
+    return false;
+  }
+  if (val == 2 || val == 3) {
+    return true;
+  }
+  if (val % 2 == 0 || val % 3 == 0) {
+    return false;
+  }
+  for (std::size_t i = 5; i * i <= val; i += 2) {
+    if (val % i == 0) {
+      return false;
+    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
+  if (n == 0) {
+    throw std::invalid_argument("n-invalid");
+  }
+  if (n == 1) {
+    return 2;
+  }
+
+  uint64_t cnt = 1;
+  uint64_t close = 3;
+
+  while (cnt < n) {
+    if (checkPrime(close)) {
+      cnt++;
+      if (cnt == n) {
+        return close;
+      }
+    }
+    close += 2;
+  }
   return 2;
 }
 
-uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+uint64_t nextPrime(uint64_t val) {
+  if (val < 2) {
+    return 2;
+  }
+
+  uint64_t closest = val + 1;
+
+  if (closest % 2 == 0) {
+    closest++;
+  }
+
+  while (!checkPrime(closest)) {
+    closest += 2;
+  }
+
+  return closest;
 }
 
-uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+uint64_t sumPrime(uint64_t upper) {
+  if (upper <= 2) {
+    return 0;
+  }
+
+  uint64_t sum = 2;
+
+  for (uint64_t i = 3; i < upper; i += 2) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+
+  return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -4,14 +4,104 @@
 #include <cstdint>
 #include "alg.h"
 
-
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(st1, 0_no) {
+  bool res = checkPrime(0);
+  EXPECT_FALSE(res);
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+TEST(st1, 1_no) {
+  bool res = checkPrime(1);
+  EXPECT_FALSE(res);
+}
+TEST(st1, 4_no) {
+  bool res = checkPrime(4);
+  EXPECT_FALSE(res);
+}
+TEST(st1, 5_yes) {
+  bool res = checkPrime(5);
+  EXPECT_TRUE(res);
+}
+TEST(st1, 23_yes) {
+  bool res = checkPrime(23);
+  EXPECT_TRUE(res);
+}
+TEST(st1, 97_yes) {
+  bool res = checkPrime(97);
+  EXPECT_TRUE(res);
+}
+TEST(st1, 100_no) {
+  bool res = checkPrime(100);
+  EXPECT_FALSE(res);
+}
+
+
+TEST(st1, n_0) {
+  EXPECT_ANY_THROW(nPrime(0));
+}
+TEST(st1, n_1) {
+  uint64_t res = nPrime(1);
+  EXPECT_EQ(2, res);
+}
+TEST(st1, n_4) {
+  uint64_t res = nPrime(4);
+  EXPECT_EQ(7, res);
+}
+TEST(st1, n_5) {
+  uint64_t res = nPrime(5);
+  EXPECT_EQ(11, res);
+}
+TEST(st1, n_10) {
+  uint64_t res = nPrime(10);
+  EXPECT_EQ(29, res);
+}
+TEST(st1, n_23) {
+  uint64_t res = nPrime(23);
+  EXPECT_EQ(83, res);
+}
+
+
+TEST(st1, next_0) {
+  uint64_t res = nextPrime(0);
+  EXPECT_EQ(2, res);
+}
+TEST(st1, next_1) {
+  uint64_t res = nextPrime(1);
+  EXPECT_EQ(2, res);
+}
+TEST(st1, next_4) {
+  uint64_t res = nextPrime(4);
+  EXPECT_EQ(5, res);
+}
+TEST(st1, next_5) {
+  uint64_t res = nextPrime(5);
+  EXPECT_EQ(7, res);
+}
+TEST(st1, next_23) {
+  uint64_t res = nextPrime(23);
+  EXPECT_EQ(29, res);
+}
+
+
+TEST(st1, sum_0) {
+  uint64_t res = sumPrime(0);
+  EXPECT_EQ(0, res);
+}
+TEST(st1, sum_1) {
+  uint64_t res = sumPrime(1);
+  EXPECT_EQ(0, res);
+}
+TEST(st1, sumt_4) {
+  uint64_t res = sumPrime(4);
+  EXPECT_EQ(5, res);
+}
+TEST(st1, sum_5) {
+  uint64_t res = sumPrime(5);
+  EXPECT_EQ(5, res);
+}
+TEST(st1, sum_23) {
+  uint64_t res = sumPrime(23);
+  EXPECT_EQ(77, res);
+}
+TEST(st1, sum_30) {
+  uint64_t res = sumPrime(30);
+  EXPECT_EQ(129, res);
 }
